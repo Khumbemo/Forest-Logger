fa4d3f8314f173dfedf3f1adb707b08156bf2089
khumbemo T Humtsoe <khumbemothumtsoe@gmail.com>
2026-03-26 01:39:37 +0530

    Create SCIENTIFIC_SYMBOLS.js with scientific Unicode symbols

---
+++ SCIENTIFIC_SYMBOLS.js
@@ -0,0 +1,11 @@
+// List of scientific Unicode symbols
+const scientificSymbols = {
+    0x2200: '\u2200', // For all
+    0x2201: '\u2201', // Complement
+    0x2202: '\u2202', // Partial differential
+    0x2203: '\u2203', // There exists
+    0x2204: '\u2204', // There does not exist
+    // ... add more scientific symbols
+};
+
+export default scientificSymbols;
